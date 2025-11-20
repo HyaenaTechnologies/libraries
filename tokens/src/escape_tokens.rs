2608ed8a44bdfdbb50dc30e4b1c4c5dbf724f817
John Matthias Wabwire <124460109+JohnMatthiasWabwire@users.noreply.github.com>
2025-11-20 01:02:41 -0700

    Remove Extra Allocations

--- tokens/src/escape_tokens.rs
+++ tokens/src/escape_tokens.rs
@@ -1,16 +1,16 @@
-use std::{primitive::str, vec::Vec};
+use std::{primitive::char, vec::Vec};
 
 // Escape Token Defintion
-pub type EscapeToken = &'static str;
+pub type EscapeToken = char;
 
 // Escape Tokens
-pub const APOSTROPHE: EscapeToken = "\'";
-pub const BACKSLASH: EscapeToken = "\\";
-pub const CARRIAGE_RETURN: EscapeToken = "\r";
-pub const NEW_LINE: EscapeToken = "\n";
-pub const NULL_CHARACTER: EscapeToken = "\0";
-pub const QUOTATION_MARK: EscapeToken = "\"";
-pub const TAB: EscapeToken = "\t";
+pub const APOSTROPHE: EscapeToken = '\'';
+pub const BACKSLASH: EscapeToken = '\\';
+pub const CARRIAGE_RETURN: EscapeToken = '\r';
+pub const NEW_LINE: EscapeToken = '\n';
+pub const NULL_CHARACTER: EscapeToken = '\0';
+pub const QUOTATION_MARK: EscapeToken = '\"';
+pub const TAB: EscapeToken = '\t';
 
 // Escape Token Vector
 pub fn escapes_vector() -> Vec<EscapeToken> {
--- tokens/src/operator_tokens.rs
+++ tokens/src/operator_tokens.rs
@@ -1,101 +1,114 @@
-use std::{primitive::str, vec::Vec};
+use std::{primitive::{str, char}, vec::Vec};
 
-// Operator Token Defintion
-pub type OperatorToken = &'static str;
+// Character Operator Token Defintion
+pub type CharacterOperatorToken = char;
 
-// Operator Tokens
-pub const ADDITION: OperatorToken = "+";
-pub const ADDITION_ASSIGNMENT: OperatorToken = "+=";
-pub const ANNOTATION: OperatorToken = "@";
-pub const ASSIGNMENT: OperatorToken = "=";
-pub const BITWISE_SHIFT_LEFT: OperatorToken = "<<";
-pub const BITWISE_SHIFT_RIGHT: OperatorToken = ">>";
-pub const BITWISE_SHIFT_LEFT_ASSIGNMENT: OperatorToken = "<<=";
-pub const BITWISE_SHIFT_RIGHT_ASSIGNMENT: OperatorToken = ">>=";
-pub const COLON: OperatorToken = ":";
-pub const COMMA: OperatorToken = ",";
-pub const DECREMENT: OperatorToken = "--";
-pub const DIVISION: OperatorToken = "/";
-pub const DIVISION_ASSIGNMENT: OperatorToken = "/=";
-pub const DOLLAR_SIGN: OperatorToken = "$";
-pub const EQUALITY: OperatorToken = "==";
-pub const EXCLUSIVE_OR: OperatorToken = "^";
-pub const FIELD_ACCESS: OperatorToken = ".";
-pub const GRAVE_ACCENT: OperatorToken = "`";
-pub const GREATER_THAN: OperatorToken = ">";
-pub const GREATER_OR_EQUAL: OperatorToken = ">=";
-pub const INCLUSIVE_RANGE: OperatorToken = "..=";
-pub const INCREMENT: OperatorToken = "++";
-pub const LAMBDA: OperatorToken = "=>";
-pub const LEFT_ARROW: OperatorToken = "<-";
-pub const LESS_THAN: OperatorToken = "<";
-pub const LESS_OR_EQUAL: OperatorToken = "<=";
-pub const LOGICAL_CONJUCTION: OperatorToken = "&";
-pub const LOGICAL_DISJUNCTION: OperatorToken = "|";
-pub const MODULUS: OperatorToken = "%";
-pub const MODULUS_ASSIGNMENT: OperatorToken = "%=";
-pub const MULTIPLICATION: OperatorToken = "*";
-pub const MULTIPLICATION_ASSIGNMENT: OperatorToken = "*=";
-pub const NEGATION: OperatorToken = "!";
-pub const NOT_EQUAL: OperatorToken = "!=";
-pub const NULL_COALESCING: OperatorToken = "?";
-pub const PATH_SEPARATOR: OperatorToken = "::";
-pub const RANGE: OperatorToken = "..";
-pub const RETURN_TYPE: OperatorToken = "->";
-pub const SEMI_COLON: OperatorToken = ";";
-pub const SUBTRACTION: OperatorToken = "-";
-pub const SUBTRACTION_ASSIGNMENT: OperatorToken = "-=";
-pub const TILDE: OperatorToken = "~";
-pub const UNDERSCORE: OperatorToken = "_";
-pub const VARIADIC_RANGE: OperatorToken = "...";
+// Character Operator Tokens
+pub const ADDITION: CharacterOperatorToken = '+';
+pub const ANNOTATION: CharacterOperatorToken = '@';
+pub const ASSIGNMENT: CharacterOperatorToken = '=';
+pub const COLON: CharacterOperatorToken = ':';
+pub const COMMA: CharacterOperatorToken = ',';
+pub const DIVISION: CharacterOperatorToken = '/';
+pub const DOLLAR_SIGN: CharacterOperatorToken = '$';
+pub const EXCLUSIVE_OR: CharacterOperatorToken = '^';
+pub const FIELD_ACCESS: CharacterOperatorToken = '.';
+pub const GRAVE_ACCENT: CharacterOperatorToken = '`';
+pub const GREATER_THAN: CharacterOperatorToken = '>';
+pub const LESS_THAN: CharacterOperatorToken = '<';
+pub const LOGICAL_CONJUCTION: CharacterOperatorToken = '&';
+pub const LOGICAL_DISJUNCTION: CharacterOperatorToken = '|';
+pub const MODULUS: CharacterOperatorToken = '%';
+pub const MULTIPLICATION: CharacterOperatorToken = '*';
+pub const NEGATION: CharacterOperatorToken = '!';
+pub const NULL_COALESCING: CharacterOperatorToken = '?';
+pub const SEMI_COLON: CharacterOperatorToken = ';';
+pub const SUBTRACTION: CharacterOperatorToken = '-';
+pub const TILDE: CharacterOperatorToken = '~';
+pub const UNDERSCORE: CharacterOperatorToken = '_';
 
-// Operator Token Vector
-pub fn operators_vector() -> Vec<OperatorToken> {
-    let operators: Vec<OperatorToken> = Vec::from([
+// String Operator Token Defintion
+pub type StringOperatorToken = &'static str;
+
+// String Operator Tokens
+pub const ADDITION_ASSIGNMENT: StringOperatorToken = "+=";
+pub const BITWISE_SHIFT_LEFT: StringOperatorToken = "<<";
+pub const BITWISE_SHIFT_RIGHT: StringOperatorToken = ">>";
+pub const BITWISE_SHIFT_LEFT_ASSIGNMENT: StringOperatorToken = "<<=";
+pub const BITWISE_SHIFT_RIGHT_ASSIGNMENT: StringOperatorToken = ">>=";
+pub const DECREMENT: StringOperatorToken = "--";
+pub const DIVISION_ASSIGNMENT: StringOperatorToken = "/=";
+pub const EQUALITY: StringOperatorToken = "==";
+pub const GREATER_OR_EQUAL: StringOperatorToken = ">=";
+pub const INCLUSIVE_RANGE: StringOperatorToken = "..=";
+pub const INCREMENT: StringOperatorToken = "++";
+pub const LAMBDA: StringOperatorToken = "=>";
+pub const LEFT_ARROW: StringOperatorToken = "<-";
+pub const LESS_OR_EQUAL: StringOperatorToken = "<=";
+pub const MODULUS_ASSIGNMENT: StringOperatorToken = "%=";
+pub const MULTIPLICATION_ASSIGNMENT: StringOperatorToken = "*=";
+pub const NOT_EQUAL: StringOperatorToken = "!=";
+pub const PATH_SEPARATOR: StringOperatorToken = "::";
+pub const RANGE: StringOperatorToken = "..";
+pub const RETURN_TYPE: StringOperatorToken = "->";
+pub const SUBTRACTION_ASSIGNMENT: StringOperatorToken = "-=";
+pub const VARIADIC_RANGE: StringOperatorToken = "...";
+
+// Character Operator Token Vector
+pub fn character_operators_vector() -> Vec<CharacterOperatorToken> {
+    let operators: Vec<CharacterOperatorToken> = Vec::from([
         ADDITION,
-        ADDITION_ASSIGNMENT,
         ANNOTATION,
         ASSIGNMENT,
-        BITWISE_SHIFT_LEFT,
-        BITWISE_SHIFT_RIGHT,
-        BITWISE_SHIFT_LEFT_ASSIGNMENT,
-        BITWISE_SHIFT_RIGHT_ASSIGNMENT,
         COLON,
         COMMA,
-        DECREMENT,
         DIVISION,
-        DIVISION_ASSIGNMENT,
         DOLLAR_SIGN,
-        EQUALITY,
         EXCLUSIVE_OR,
         FIELD_ACCESS,
         GRAVE_ACCENT,
         GREATER_THAN,
+        LESS_THAN,
+        LOGICAL_CONJUCTION,
+        LOGICAL_DISJUNCTION,
+        MODULUS,
+        MULTIPLICATION,
+        NEGATION,
+        NULL_COALESCING,
+        SEMI_COLON,
+        SUBTRACTION,
+        TILDE,
+        UNDERSCORE
+    ]);
+
+    return operators;
+}
+
+// String Operator Token Vector
+pub fn string_operators_vector() -> Vec<StringOperatorToken> {
+    let operators: Vec<StringOperatorToken> = Vec::from([
+        ADDITION_ASSIGNMENT,
+        BITWISE_SHIFT_LEFT,
+        BITWISE_SHIFT_RIGHT,
+        BITWISE_SHIFT_LEFT_ASSIGNMENT,
+        BITWISE_SHIFT_RIGHT_ASSIGNMENT,
+        DECREMENT,
+        DIVISION_ASSIGNMENT,
+        EQUALITY,
         GREATER_OR_EQUAL,
         INCLUSIVE_RANGE,
         INCREMENT,
         LAMBDA,
         LEFT_ARROW,
-        LESS_THAN,
         LESS_OR_EQUAL,
-        LOGICAL_CONJUCTION,
-        LOGICAL_DISJUNCTION,
-        MODULUS,
         MODULUS_ASSIGNMENT,
-        MULTIPLICATION,
         MULTIPLICATION_ASSIGNMENT,
-        NEGATION,
         NOT_EQUAL,
-        NULL_COALESCING,
         PATH_SEPARATOR,
         RANGE,
         RETURN_TYPE,
-        SEMI_COLON,
-        SUBTRACTION,
         SUBTRACTION_ASSIGNMENT,
-        TILDE,
-        UNDERSCORE,
-        VARIADIC_RANGE,
+        VARIADIC_RANGE
     ]);
 
     return operators;

6b48b633f5a5f5e5dd4ffce4593f7475a6914e58
John Matthias Wabwire <124460109+JohnMatthiasWabwire@users.noreply.github.com>
2026-01-06 19:37:43 -0700

    Update Allocators Library

---
+++ allocators/src/dynamic_array.c
@@ -0,0 +1,73 @@
+#include "./dynamic_array.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Changes the Size of the Dynamic Array
+int change_size(struct DynamicArray array) {
+
+
+  return 0;
+}
+
+// Clear all Elements from the Dynamic Array
+int clear_array(struct DynamicArray array) {
+
+
+  return 0;
+}
+
+// Create Dynamic Array
+int create_array(struct DynamicArray array) {
+
+
+  return 0;
+}
+
+// Insert an Element into the Dynamic Array
+// At an Index Position
+// Shifts all Elements after it, to the Right
+int insert_element(struct DynamicArray array, int index) {
+
+
+  return 0;
+}
+
+// Append an Element to the End of the Dynamic Array
+int push_element(struct DynamicArray array) {
+
+
+  return 0;
+}
+
+// Remove an Element from of the Dynamic Array
+// At an Index Position
+// Returns the Element
+// Shifts all Elements after it, to the Left
+int remove_element(struct DynamicArray array, int index) {
+
+
+  return 0;
+}
+
+// Returns the current Index of the Dynamic Array
+int return_index(struct DynamicArray array) {
+
+
+  return 0;
+ }
+
+// Returns the Length of the Dynamic Array
+int return_length(struct DynamicArray array) {
+
+
+  return 0;
+}
+
+// Returns the Size of the Buffer of the Dynamic Array
+int return_size(struct DynamicArray array) {
+
+
+  return 0;
+}
+

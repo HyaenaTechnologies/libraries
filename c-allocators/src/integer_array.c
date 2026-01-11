aef182b590d8d82661cf5f1a5f94cf6e763bca06
John Matthias Wabwire <124460109+JohnMatthiasWabwire@users.noreply.github.com>
2026-01-11 01:55:54 -0700

    Continue Refactor

--- c-allocators/src/integer_array.c
+++ c-allocators/src/integer_array.c
@@ -5,6 +5,10 @@
 
 // Clear all Elements from the Dynamic Integer Array
 int clear_array(struct IntegerArray initial_array) {
+  int **array = initial_array.array;
+  int capacity = initial_array.capacity;
+  int index = initial_array.index;
+  int length = initial_array.length;
 
 
   return 0;
@@ -19,17 +23,17 @@ int create_array(struct IntegerArray initial_array) {
 
   if (capacity == 0) {
     capacity = 4;
-    array = malloc(sizeof(int[capacity+length]));
+    array = malloc(sizeof(int[capacity]));
   } else if (capacity != 0) {
-    array = malloc(sizeof(int[capacity+length]));
+    array = malloc(sizeof(int[capacity]));
   }
 
   if (array == NULL) {    
-    printf(stderr, "Error Allocating Memory\n");
-  } else {
-    
-  }  
-    
+    printf(stderr, "Memory Allocation Error\n");
+  } else if (length == capacity) {
+    array = realloc(array,sizeof(int[(capacity+length)*2]));
+  }
+
   return 0;
 }
 
@@ -37,6 +41,9 @@ int create_array(struct IntegerArray initial_array) {
 // At an Index Position
 // Shifts all Elements after it, to the Right
 int insert_element(struct IntegerArray initial_array, int index) {
+  int **array = initial_array.array;
+  int capacity = initial_array.capacity;
+  int length = initial_array.length;
 
 
   return 0;
@@ -44,6 +51,10 @@ int insert_element(struct IntegerArray initial_array, int index) {
 
 // Append an Element to the End of the Dynamic Integer Array
 int push_element(struct IntegerArray initial_array) {
+  int **array = initial_array.array;
+  int capacity = initial_array.capacity;
+  int index = initial_array.index;
+  int length = initial_array.length;
 
 
   return 0;
@@ -54,8 +65,19 @@ int push_element(struct IntegerArray initial_array) {
 // Returns the Element
 // Shifts all Elements after it, to the Left
 int remove_element(struct IntegerArray initial_array, int index) {
+  int **array = initial_array.array;
+  int capacity = initial_array.capacity;
+  int element;
+  int length = initial_array.length;
 
+  for (element = index; element < length; element = element + 1) {
+    array[element] = array[element + 1];
+  }
 
-  return 0;
+  capacity = capacity -1;
+  length = length - 1;
+  array = realloc(array,sizeof(int[(capacity)]));
+
+  return *array[index];
 }
 
